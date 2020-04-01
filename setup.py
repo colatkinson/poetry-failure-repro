94320fc5e10a48f89bea919f8d9b2781ebbb162a
Colin Atkinson <colin.william.atkinson@gmail.com>
2020-03-31 21:44:18 -0400

    Single setup()

--- setup.py
+++ setup.py
@@ -6,16 +6,6 @@ setuptools.setup(
     author="Colin Atkinson",
     author_email="colin@atakama.com",
     description="A small example package",
-    packages=["example_pkg"],
-    python_requires='>=3.6',
-)
-
-setuptools.setup(
-    name="another-pkg",
-    version="0.0.1",
-    author="Colin Atkinson",
-    author_email="colin@atakama.com",
-    description="A small example package",
-    packages=["another_pkg"],
+    packages=["example_pkg", "another_pkg"],
     python_requires='>=3.6',
 )
