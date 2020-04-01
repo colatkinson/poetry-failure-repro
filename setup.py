52718c7d732ad24a015802a22c4fbfb795f029c1
Colin Atkinson <colin.william.atkinson@gmail.com>
2020-03-31 20:53:22 -0400

    make package decl dynamic

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 import setuptools
 
-setuptools.setup(
-    name="example-pkg",
-    version="0.0.1",
-    author="Colin Atkinson",
-    author_email="colin@atakama.com",
-    description="A small example package",
-    packages=["example_pkg"],
-    python_requires='>=3.6',
-)
+def fn():
+    return True
+
+if fn():
+    setuptools.setup(
+        name="example-pkg",
+        version="0.0.1",
+        author="Colin Atkinson",
+        author_email="colin@atakama.com",
+        description="A small example package",
+        packages=["example_pkg"],
+        python_requires='>=3.6',
+    )
 
 setuptools.setup(
     name="another-pkg",
