2c480e758ae137f4f7eb8eeb323f298044c59a10
Colin Atkinson <colin.william.atkinson@gmail.com>
2020-03-31 21:21:06 -0400

    Revert "make package decl dynamic"
    This reverts commit 52718c7d732ad24a015802a22c4fbfb795f029c1.

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 import setuptools
 
-def fn():
-    return True
-
-if fn():
-    setuptools.setup(
-        name="example-pkg",
-        version="0.0.1",
-        author="Colin Atkinson",
-        author_email="colin@atakama.com",
-        description="A small example package",
-        packages=["example_pkg"],
-        python_requires='>=3.6',
-    )
+setuptools.setup(
+    name="example-pkg",
+    version="0.0.1",
+    author="Colin Atkinson",
+    author_email="colin@atakama.com",
+    description="A small example package",
+    packages=["example_pkg"],
+    python_requires='>=3.6',
+)
 
 setuptools.setup(
     name="another-pkg",
